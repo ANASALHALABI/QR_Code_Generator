96a4810883974a9d09cecbec0774945fde53c561
Anas Alhalabi <you@example.com>
2024-06-20 09:45:14 -0700

    add project

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let imgBox: any = document.getElementById("imgBox");
+let qrImage: any = document.getElementById("qrImage");
+let qrText: any = document.getElementById("qrText");
+
+function generateQrCode(){
+    if(qrText.value.length > 0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+        + qrText.value;
+        imgBox.classList.add("show-img");
+    }else{
+        qrText.classList.add("error");
+        setTimeout(() =>{
+            qrText.classList.remove("error");
+        },1000)
+    }
+    
+}
