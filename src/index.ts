70fe34d5a9c2852e040265764b61673c4a107819
Anas Alhalabi <you@example.com>
2024-06-20 14:27:15 -0700

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ let imgBox: any = document.getElementById("imgBox");
 let qrImage: any = document.getElementById("qrImage");
 let qrText: any = document.getElementById("qrText");
 
-function generateQrCode(){
+function generateQrCode(): void{
     if(qrText.value.length > 0){
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
         + qrText.value;
